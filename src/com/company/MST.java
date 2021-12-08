477493e2e057469a0f0a2f2ea0937dbb57b76f7b
meirson1 <noam220599@gmail.com>
2021-12-08 18:12:44 +0200

    fix linked list

--- src/com/company/MST.java
+++ src/com/company/MST.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.*;
 public class MST {
 
     private static final int V = 4;
@@ -77,9 +79,69 @@ public class MST {
 
     void NewMst(int[] parents, int[][] graph, int[] saveIndexI, int[] saveIndexJ)
     {
-        boolean flag=false;
-        for (int i = 1; i < parents.length; i++) {
-
+        ArrayList<ArrayList<Integer>> adjListArray = convert(graph);
+        LinkedList<Integer> circle=FindCircleDfs(adjListArray);
+        int max=0;
+        int indexI=0;
+        int indexJ=0;
+        for (int i = 0; i < circle.size(); i++) {
+            if (max > graph[circle.get(i)][circle.get(i+1)]) {
+                max = graph[circle.get(i)][circle.get(i+1)];
+                indexI=circle.get(i);
+                indexJ=circle.get(i+1);
+            }
         }
+        removeEdge(graph,max,indexI,indexJ);
+        printGraph(graph);
+    }
+
+    private void printGraph(int[][] graph) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                System.out.println(graph[i][j]);
+            }
+        }
+    }
+
+    public static ArrayList<ArrayList<Integer>> convert(int[][] graph) {
+        int l = graph[0].length;
+        ArrayList<ArrayList<Integer>> adjListArray = new ArrayList<ArrayList<Integer>>(l);
+
+        // Create a new list for each
+        // vertex such that adjacent
+        // nodes can be stored
+        for (int i = 0; i < l; i++) {
+            adjListArray.add(new ArrayList<Integer>());
+        }
+
+        for (int i = 0; i < graph[0].length; i++) {
+            for (int j = 0; j < graph.length; j++) {
+                if (graph[i][j] != 0) {
+                    adjListArray.get(i).add(j);
+                }
+            }
+        }
+
+        return adjListArray;
+    }
+
+    public LinkedList<Integer> FindCircleDfs(ArrayList<ArrayList<Integer>> adjListArray) {
+        LinkedList<Integer> circle=new LinkedList<Integer>();
+        circle.add(0);
+        int i=0;
+        int j=0;
+        while (adjListArray.get(i).get(j)!=null) {
+            if (!circle.contains(adjListArray.get(i).get(j))) {
+                circle.add(adjListArray.get(i).get(j));
+                i=adjListArray.get(i).get(j);
+            }else
+                j++;
+        }
+        return circle;
+    }
+
+    public void removeEdge(int[][] graph,int max,int i,int j){
+        graph[i][j]=0;
+        graph[j][i]=0;
     }
 }
